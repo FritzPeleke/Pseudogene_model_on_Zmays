0b6d0885edff78c79f0c8e731f0fb7ae2a77583f
Fritz Peleke <fritzforbang160@gmail.com>
2020-06-16 14:42:31 +0200

    other projects

--- VCF_GTF_adjust.py
+++ VCF_GTF_adjust.py
@@ -1,6 +1,7 @@
 import pyranges as pr
 import itertools
 import pandas as pd
+import os
 pd.options.display.width = 0
 
 
@@ -10,6 +11,7 @@ def adjust_vcf(file, chrom_size):
     with open(file, 'r') as read_obj:
         for line in itertools.islice(read_obj, 7, None):
             records.append(list(line.split('\t')))
+    print('Processing vcf')
     df = pd.DataFrame(records)
     df.columns = df.iloc[0]
     df.drop(0, inplace=True)
@@ -25,17 +27,16 @@ def adjust_vcf(file, chrom_size):
     return vcf
 
 
-vcf = adjust_vcf('intersection_10001.vcf', 5)
-
-
-def adjust_gtf(file, vcf_file):
+def adjust_gtf(file, vcf_file, new_file):
     vcf = vcf_file
     df = pr.read_gtf(file).df
     df = df[df['Feature'] == 'gene']
     df = df[df['gene_biotype'] == 'protein_coding']
     df = df[['Chromosome', 'Source', 'Feature', 'Start', 'End', 'Score', 'Strand', 'gene_id']]
-    df = df[df['Chromosome'].isin(['1', '2', '3', '4', '5'])]# To ensure we use just chromosome 1-5, excluding Mt and Pt
+    # To ensure we use just chromosome 1-5, excluding Mt and Pt
+    df = df[df['Chromosome'].isin(['1', '2', '3', '4', '5'])]
     df.reset_index(inplace=True, drop=True)
+    print('processing gtf')
     for idx, shift in enumerate(vcf['SHIFT']):
         if shift != 0:
             position = int(vcf['POS'][idx])
@@ -49,7 +50,13 @@ def adjust_gtf(file, vcf_file):
                         df.loc[chr[0], 'End'] = df.loc[chr[0], 'End'] + shift
                     elif start < position < end:
                         df.loc[chr[0], 'End'] = df.loc[chr[0], 'End'] + shift
-    df.to_csv('variant.gtf', header=False, index=False, sep='\t')
+    df.to_csv(new_file, header=False, index=False, sep='\t')
 
 
-adjust_gtf('Arabidopsis_thaliana.TAIR10.46.gtf', vcf)
+for file in os.listdir('/nam-99/ablage/nam/peleke/vcf_files'):
+    vcf_path = '/nam-99/ablage/nam/peleke/vcf_files/' + file
+    vcf = adjust_vcf(vcf_path, 5)
+    ecotype_id = file.split('_')[1].split('.')[0]
+    file_name_new_gtf = '/nam-99/ablage/nam/peleke/variant_gtfs/' + ecotype_id + '.gtf'
+    ref_gtf = '/nam-99/ablage/nam/peleke/Arabidopsis_thaliana.TAIR10.46.gtf'
+    adjust_gtf(ref_gtf, vcf, file_name_new_gtf)
